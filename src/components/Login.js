8a3cbfb3b832f848921f67f79211d5bc3f47ac26
Nathan Hall <nathan.mh@gmail.com>
2017-04-21 17:14:29 -0400

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,37 +1,38 @@
 // https://github.com/FaridSafi/react-native-google-places-autocomplete
 import React, { Component } from 'react';
 import Expo from 'expo';
-import { AsyncStorage } from 'react-native';
-// import Auth0Lock from 'react-native-lock';
+import { AsyncStorage, WebView,View, StyleSheet } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
 import { AUTH0_CLIENT_ID, AUTH0_DOMAIN } from '../../config/auth0';
-import { loginUser } from '../services/apiActions';
 
 const auth0ClientId = 'yVWgohF5HglLD5qTqv1zols99eHPYlBK'
 const auth0Domain = 'https://dominathan.auth0.com';
 const redirectUri = 'exp://ek-n3k.dominathan.iosrayka.exp.direct/+/redirect';
 
 export class Login extends Component {
-
-  componentDidMount() {
-    if (this.props.getIsLoggedIn()) {
-      Actions.home({ type: 'reset' });
-    } else {
-      this._loginWithAuth0()
+  constructor(props) {
+    super(props)
+    this.state = {
+      url: `${auth0Domain}/authorize` + this.createQueryString()
     }
   }
 
-  _loginWithAuth0 = async () => {
-    console.log("SHOW SCREEN")
-    const redirectionURL = `${auth0Domain}/authorize` + this._toQueryString({
+  componentDidMount() {
+    console.log("IS LOGGED IN", this.props.getIsLoggedIn())
+    if (this.props.getIsLoggedIn()) {
+      Actions.home({ type: 'reset' });
+    }
+  }
+
+  createQueryString() {
+    return this._toQueryString({
       client_id: auth0ClientId,
       response_type: 'token',
       scope: 'openid name profile email',
       redirect_uri: redirectUri,
       state: redirectUri,
     });
-    Expo.WebBrowser.openBrowserAsync(redirectionURL);
   }
 
   _toQueryString(params) {
@@ -40,8 +41,31 @@ export class Login extends Component {
       .join('&');
   }
 
+  onNavigationStateChange(navState) {
+    console.log("HAPPENING", navState)
+    if(navState['url'].includes('/+/redirect')) {
+      Actions.home({type: 'reset'})
+    }
+  }
 
   render() {
-    return (null);
+    const { url } = this.state;
+    return (
+      <View style={styles.container}>
+        {url && <WebView
+          source={{uri: url}}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          onNavigationStateChange={this.onNavigationStateChange}
+
+        />}
+      </View>
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
